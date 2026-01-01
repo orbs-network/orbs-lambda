bbe0c257f9ebf020b20caaddde0342a5d1450f15
uvorbs <yuval@orbs.com>
2026-01-01 14:19:05 +0200

    fix proj name

--- projects/cmt-sync/index.js
+++ projects/cmt-sync/index.js
@@ -115,8 +115,9 @@ async function getSignedCommittee(args) {
 
 module.exports.register = function (engine) {
   // get current file's directory - but just the last bit of the path  (so we can use it as the projectName)
-  const projName = __filename.split('/').pop().split('.')[0]
-  console.log("currentDirectory:", projName)
+  const path = require('path')
+  const projName = path.basename(path.dirname(__filename))
+  console.log("register project Name: ", projName)
   // projectName has to be the same as the folder name
 
   engine.onRpc(getCurrentCommittee, { projectName: projName, taskName: "getCurrentCommittee" });
