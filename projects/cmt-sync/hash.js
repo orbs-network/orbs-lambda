9cb9b0c78e27212deddee39d4c8dfa43bc56762d
uvorbs <yuval@orbs.com>
2026-01-29 15:19:54 +0200

    restore hash

--- projects/cmt-sync/hash.js
+++ projects/cmt-sync/hash.js
@@ -1,5 +1,29 @@
 const Web3 = require('web3');
+const web3 = new Web3();
 
+const NAME = "OrbsCommitteeSync";
+const VERSION = "0";
+
+const EIP712_DOMAIN_TYPE = "EIP712Domain(string name,string version)";
+const CONFIG_TYPE = "Config(address account,uint8 version,bytes value)";
+const DIGEST_TYPE = "Digest(uint256 nonce,address[] committee,Config[] config)";
+
+const strip0x = Web3.utils.stripHexPrefix;
+const kUtf8 = (s) => !s || s.length === 0 || s === '0x' ? "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470" : Web3.utils.keccak256(Web3.utils.utf8ToHex(s));
+const k = (s) => !s || s.length === 0 || s === '0x' ? "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470" : Web3.utils.keccak256(s);
+
+const pack32 = (xs) => "0x" + xs.map((x) => strip0x(Web3.utils.padLeft(x, 64))).join("");
+const addr32 = (a) => Web3.utils.padLeft(a, 64);
+
+const EIP712_DOMAIN_SEPARATOR = k(
+  web3.eth.abi.encodeParameters(
+    ["bytes32", "bytes32", "bytes32"],
+    [kUtf8(EIP712_DOMAIN_TYPE), kUtf8(NAME), kUtf8(VERSION)]
+  )
+);
+
+const CONFIG_TYPEHASH = kUtf8(CONFIG_TYPE);
+const DIGEST_TYPEHASH = kUtf8(DIGEST_TYPE + CONFIG_TYPE);
 
 function hashCommittee(committee) {
   const pk = pack32(committee.map(addr32))
@@ -42,5 +66,6 @@ module.exports = {
 // debug
 // const configElement = ['0x3333333471138EF42aD64829227C6cd8f1F9F47a', 123, '0x3333333471138EF42aD64829227C6cd8f1F9F47a3333333471138EF42aD64829227C6cd8f1F9F47a']
 // const committee = ['0x3333333471138EF42aD64829227C6cd8f1F9F47a', '0x3333333471138EF42aD64829227C6cd8f1F9F47a'.toLowerCase()];
-// const result = hash(123, committee, [configElement, configElement, configElement])
+// //const result = hash(1, committee, [configElement, configElement, configElement])
+// const result = hash(1, [], [])
 // console.log("result: ", result);  
